199734917879b7707a8629d3725778ce2e5a24e6
ampuriraelizabeth <elizabethampurira@gmail.com>
2022-06-19 15:48:47 +0300

    binding

--- app/src/main/java/dev/liz/workoutlog/SignupActivity.kt
+++ app/src/main/java/dev/liz/workoutlog/SignupActivity.kt
@@ -8,67 +8,48 @@ import android.widget.EditText
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import dev.liz.workoutlog.databinding.ActivityLoginBinding
+import dev.liz.workoutlog.databinding.ActivitySignupBinding
 
 
 class SignupActivity : AppCompatActivity() {
-    lateinit var btnSignup:Button
-    lateinit var tilFirstname:TextInputLayout
-    lateinit var tilSecondname:TextInputLayout
-    lateinit var tilEmail2:TextInputLayout
-    lateinit var tilComfirm:TextInputLayout
-    lateinit var etFirstname:EditText
-    lateinit var etSecondname:EditText
-    lateinit var etEmail2:EditText
-    lateinit var etComfirm:EditText
-    lateinit var tvLog2:TextView
-    lateinit var tilPassword2:TextInputLayout
-    lateinit var etPassword2:EditText
+    lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
-        btnSignup=findViewById(R.id.btnSignup)
-        tilFirstname=findViewById(R.id.tilFirstname)
-        tilSecondname=findViewById(R.id.tilSecondname)
-        tilEmail2=findViewById(R.id.tilEmail2)
-        tilComfirm=findViewById(R.id.tilComfirm)
-        etFirstname=findViewById(R.id.etFirstname)
-        etSecondname=findViewById(R.id.etSecondname)
-        etEmail2=findViewById(R.id.etEmail2)
-        etComfirm=findViewById(R.id.etComfirm)
-        etPassword2=findViewById(R.id.etPassword2)
-        tvLog2=findViewById(R.id.tvLog2)
-        tilPassword2=findViewById(R.id.tilPassword2)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        tvLog2.setOnClickListener {
+
+        binding.tvLog2.setOnClickListener {
             var intent=Intent(this,LoginActivity::class.java)
             startActivity(intent)
         }
 
-        btnSignup.setOnClickListener {
+        binding.btnSignup.setOnClickListener {
             validate()
         }
     }
     fun validate(){
-        var firstname= etFirstname.text.toString()
-        var secondname= etSecondname.text.toString()
-        var email= etEmail2.text.toString()
-        var password= etPassword2.text.toString()
-        var comfirm= etComfirm.text.toString()
+        var firstname= binding.etFirstname.text.toString()
+        var secondname= binding.etSecondname.text.toString()
+        var email= binding.etEmail2.text.toString()
+        var password= binding.etPassword2.text.toString()
+        var comfirm= binding.etComfirm.text.toString()
 
         if (firstname.isBlank()) {
-            tilFirstname.error = "First name required"
+            binding.tilFirstname.error = "First name required"
         }
         if (secondname.isBlank()) {
-            tilSecondname.error = "Second name required"
+            binding.tilSecondname.error = "Second name required"
         }
         if (email.isBlank()) {
-            tilEmail2.error = "Email Address is required"
+            binding.tilEmail2.error = "Email Address is required"
         }
         if (password.isBlank()) {
-            tilPassword2.error = "Please enter password"
+            binding.tilPassword2.error = "Please enter password"
         }
         if (comfirm!=password) {
-            tilComfirm.error = "Email does not Match"
+            binding.tilComfirm.error = "Email does not Match"
         }
     }
 
